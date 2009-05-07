736bf3d7abb5c9c40a30f7990f14013cb08a307f
TJ Vanderpoel <bougyman@zero.(none)>
2009-05-07 11:54:52 -0500

    completed refactoring to methodized matchers in #parse

--- lib/name_parse/parser.rb
+++ lib/name_parse/parser.rb
@@ -11,7 +11,46 @@ module NameParse
       @first_name_re = @options[:first_name_re] || /\w[-.'\w]+/
       parse(name_string) if name_string
     end
+
+    def parse(name)
+      @raw = name
+      case name
+      # just "Firstname Lastname"
+      when first_last
+        first_last($~)
+      # Catch names with prefixes, no comma
+      when pre_first_last
+        pre_first_last($~)
+      # Catch names with prefixes and middle names, no comma
+      when pre_first_mid_last
+        pre_first_mid_last($~)
+      # just "Firstname Middle Lastname"
+      when first_mid_last
+        first_mid_last($~)
+      # just "Lastname, Firstname (Middle)" middle is optional
+      when last_comma_first_mid
+        last_comma_first_mid($~)
+      # Comma with lots of lastnames, a first name, optional middle name
+      when multi_last_comma
+        multi_last_comma($~)
+      else 
+        if @options[:raise_on_unknown]
+          raise UnknownFormat, "Could not parse #{@raw}"
+        else
+          @matched = :unknown
+          false
+        end
+      end
+    end
+
+    alias :first_name :first
+
+    def middle_i
+      return nil if middle.nil?
+      middle[0,1]
+    end
     
+    private
     def first_last(match = nil)
       return %r{^(#{first_name_re})\s+(#{last_name_re})$} if match.nil?
       @first, @last = match[1 .. 2]
@@ -30,45 +69,22 @@ module NameParse
       @matched = :pre_first_mid_last
     end
 
-    def parse(name)
-      @raw = name
-      case name
-      # just "Firstname Lastname"
-      when first_last
-        first_last($~)
-      # Catch names with prefixes, no comma
-      when pre_first_last
-        pre_first_last($~)
-      # Catch names with prefixes and middle names, no comma
-      when pre_first_mid_last
-        pre_first_mid_last($~)
-      # just "Firstname Middle Lastname"
-      when /^(#{first_name_re})\s+(#{middle_name_re})\s+(#{last_name_re})$/
-        @first, @middle, @last = $1, $2, $3
-        @matched = :first_mid_last
-      # just "Lastname, Firstname (Middle)" middle is optional
-      when /^(#{last_name_re}),(?:\s+)?(#{first_name_re})(?:\s+(#{middle_name_re}))?$/
-        @first, @last, @middle = $2, $1, $3
-        @matched = :last_comma_first_mid
-      # Comma with lots of lastnames, a first name, optional middle name
-      when /^((?:#{last_name_re}(?:\s+)?)+),(?:\s+)(#{first_name_re})(?:\s+(#{middle_name_re}))?$/
-        @first, @last, @middle = $2, $1, $3
-        @matched = :multi_last_comma
-      else 
-        if @options[:raise_on_unknown]
-          raise UnknownFormat, "Could not parse #{@raw}"
-        else
-          @matched = :unknown
-          false
-        end
-      end
+    def first_mid_last(match = nil)
+      return %r{^(#{first_name_re})\s+(#{middle_name_re})\s+(#{last_name_re})$} if match.nil?
+      @first, @middle, @last = match[1 .. 3]
+      @matched = :first_mid_last
     end
 
-    alias :first_name :first
+    def last_comma_first_mid(match = nil)
+      return %r{^(#{last_name_re}),(?:\s+)?(#{first_name_re})(?:\s+(#{middle_name_re}))?$} if match.nil?
+      @first, @last, @middle = match[2], match[1], match[3]
+      @matched = :last_comma_first_mid
+    end
 
-    def middle_i
-      return nil if middle.nil?
-      middle[0,1]
+    def multi_last_comma(match = nil)
+      return %r{^((?:#{last_name_re}(?:\s+)?)+),(?:\s+)(#{first_name_re})(?:\s+(#{middle_name_re}))?$} if match.nil?
+      @first, @last, @middle = match[2], match[1], match[3]
+      @matched = :multi_last_comma
     end
 
   end
